f8a5f8e26e28d87e86622f0d90c9c28d575d9640
Tim <tim@reach-x.com>
2018-07-06 21:45:55 -0700

    Added Core/Hash.

---
+++ app/Core/Hash/Salt.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ *
+ * @copyright (c) 2015 osCommerce; http://www.oscommerce.com
+ * @license BSD; http://www.oscommerce.com/bsdlicense.txt
+ */
+
+namespace OSC\Core\Hash;
+
+class Salt
+{
+    public static function get(string $string): string
+    {
+        $hash = '';
+
+        for ($i = 0; $i < 10; $i++) {
+            $hash .= random_int(PHP_INT_MIN, PHP_INT_MAX);
+        }
+
+        $salt = substr(md5($hash), 0, 2);
+
+        $hash = md5($salt . $string) . ':' . $salt;
+
+        return $hash;
+    }
+
+    public static function validate(string $plain, string $hashed): bool
+    {
+        if (empty($plain) || empty($hashed)) {
+            return false;
+        }
+
+// split apart the hash / salt
+        $stack = explode(':', $hashed);
+
+        if (count($stack) != 2) {
+            return false;
+        }
+
+        return (md5($stack[1] . $plain) == $stack[0]);
+    }
+
+    public static function canUse(): bool
+    {
+        return true;
+    }
+}
